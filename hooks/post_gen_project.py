6186a1322680234b907c4cca1a51b62ae5043f5d
alexgustafson <alex_gustafson@yahoo.de>
2015-08-03 19:38:13 +0200

    adding grunt watch

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,12 +4,9 @@
 import shutil
 import os
 
-
-
-
-
 os.system("git init")
 os.system("git add .")
 os.system("git commit -m 'initial commit'")
-os.system("cd {{cookiecutter.project_name}}/ && bower install")
-os.system("cd {{cookiecutter.project_name}}/ && npm install")
+os.system("bower install")
+os.system("npm install")
+os.system("grunt")

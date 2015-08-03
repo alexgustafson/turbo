f74fa36f9735b42513366343d1f4332cf4237338
alexgustafson <alex_gustafson@yahoo.de>
2015-08-04 00:27:30 +0200

    fixing watcher and sass compiler

--- {{cookiecutter.project_name}}/Gruntfile.js
+++ {{cookiecutter.project_name}}/Gruntfile.js
@@ -27,25 +27,41 @@ module.exports = function(grunt) {
       }
     },
     sass: {
-        options: {
-            sourceMap: true
-        },
-        compile: {
-          cwd: 'sass',
-          src: ['src/sass/*.scss'],
-          dest: 'dest/css/',
+      app: {
+        // Takes every file that ends with .scss from the scss 
+        // directory and compile them into the css directory. 
+        // Also changes the extension from .scss into .css. 
+        // Note: file name that begins with _ are ignored automatically
+        files: [{
+          expand: true,
+          cwd: 'src/sass',
+          src: ['*.scss'],
+          dest: 'dist/css',
           ext: '.css'
-        }
-    },
-    watch: {
-      scripts: {
-        files: ['**/*.scss'],
-        tasks: ['default'],
-        options: {
-          spawn: false
-        }
+        }]
+      },
+      options: {
+        sourceMap: true, 
+        outputStyle: 'nested', 
+        imagePath: "../",
       }
-    }
+    },
+    // Grunt-contrib-watch
+    watch: {
+      sass: {
+        // Watches all Sass or Scss files within the scss folder and one level down. 
+        // If you want to watch all scss files instead, use the "**/*" globbing pattern
+        files: ['src/sass/{,*/}*.{scss,sass}'],
+        // runs the task `sass` whenever any watched file changes 
+        tasks: ['sass']
+      },
+      options: {
+        // Sets livereload to true for livereload to work 
+        // (livereload is not covered in this article)
+        livereload: true,
+        spawn: false
+      }
+    },
   });
 
   // Load the plugin that provides the "uglify" task.
@@ -55,6 +71,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify', 'jade', 'sass']);
+  grunt.registerTask('default', ['uglify', 'jade', 'sass', 'watch']);
 
 };
